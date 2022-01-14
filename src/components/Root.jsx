c2691da276188a3ea4907d6be424468fb47e7acc
mncampos <mncampos@inf.ufrgs.br>
2022-01-14 00:12:02 -0300

    Added card prototype

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,11 +1,13 @@
 
 import '../styles/Root.css';
 import MainMenu from './MainMenu.jsx';
+import ListCard from './ListCard';
 
 function Root() {
   return (
     <div className="Root">
       <MainMenu/>
+      <ListCard/>
     </div>
   );
 }
